bdeff0b50b834e75712655b4b78ec2aa74233419
Đỗ Ngọc <dominhngocbx6801@gmail.com>
2023-11-10 00:22:33 +0700

    Update

---
+++ src/main/java/official/sellandbuymotobike/controller/HomeController.java
@@ -0,0 +1,9 @@
+package official.sellandbuymotobike.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@AllArgsConstructor
+public class HomeController {
+}
